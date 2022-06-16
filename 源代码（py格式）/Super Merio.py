5d70dfaf29e1cf50ed25f6a83f29686fd1a848f1
Thedustye <107158168+Thedustye@users.noreply.github.com>
2022-06-16 21:37:14 +0800

    Add files via upload
    python超级马里奥源代码

---
+++ 源代码（py格式）/Super Merio.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+_author_='justinarmstrong'
+
+"""
+
+This is an attempt to recreate the first level of
+Super Mario Bros for the NES
+"""
+
+
+import sys
+import pygame as pg
+from data.main import __main
+import cProfile
+
+
+
+if_name_=='_main_'
+  main()
+  pg.quit()
+  sys.exit()
